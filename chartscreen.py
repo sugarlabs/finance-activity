sugarlabs/finance-activity

commit 1c21ce44b81831132aaa520fdde4e8eccce1b705

pep8ized
Fixed a lot of code style related with PEP8

Signed-off-by: Manuel Kaufmann <humitos@gmail.com>

chartscreen.py
@@ -1,38 +1,39 @@
-# Copyright 2008 by Wade Brainerd.  
+# Copyright 2008 by Wade Brainerd.
 # This file is part of Finance.
 #
 # Finance is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Finance is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Finance.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import standard Python modules.
-import logging, os, math, time, copy, json, time, datetime, locale
-from gettext import gettext as _
+import math
+import locale
 
-# Set up localization.
-locale.setlocale(locale.LC_ALL, '')
+# Import activity module
+import finance
+
+from gettext import gettext as _
 
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
 
-# Import Sugar UI modules.
-import sugar3.activity.activity
-from sugar3.graphics import *
+# Set up localization.
+locale.setlocale(locale.LC_ALL, '')
 
-# Import activity module
-import finance
+CHART_HELP = _(
+    'The Chart view shows the proportion of your expenses that is in each '
+    'category.\nYou can categorize transactions in the Register view.')
 
-CHART_HELP = _('The Chart view shows the proportion of your expenses that is in each category.\nYou can categorize transactions in the Register view.')
 
 class ChartScreen(Gtk.HBox):
     def __init__(self, activity):
@@ -47,7 +48,7 @@ class ChartScreen(Gtk.HBox):
         self.area.connect('draw', self.chart_draw_cb)
 
         label = Gtk.Label()
-        label.set_markup('<b>'+_('Debit Categories')+'</b>')
+        label.set_markup('<b>' + _('Debit Categories') + '</b>')
 
         self.catbox = Gtk.VBox()
 
@@ -60,28 +61,29 @@ class ChartScreen(Gtk.HBox):
         box.pack_start(self.catbox, False, False, 10)
         box.pack_start(Gtk.Box(orientation=Gtk.Orientation.VERTICAL),
                        True, True, 0)
- 
+
         self.pack_start(self.area, True, True, 0)
         self.pack_start(box, False, False, 40)
 
         self.show_all()
-       
+
     def build(self):
         # Build the category totals.
         self.category_total = {}
         for t in self.activity.visible_transactions:
             cat = t['category']
             amount = t['amount']
-            
+
             if t['type'] == 'debit':
-                if not self.category_total.has_key(cat):
+                if not cat self.category_total:
                     self.category_total[cat] = amount
-                else: 
-                    self.category_total[cat] += amount 
+                else:
+                    self.category_total[cat] += amount
 
         # Generate a list of names sorted by total.
         self.sorted_categories = self.category_total.keys()
-        #self.sorted_categories.sort(lamba a, b: cmp(self.category_total[a], self.category_total[b]))
+        # self.sorted_categories.sort(lamba a, b: cmp(self.category_total[a],
+        #                                             self.category_total[b]))
 
         # Clear and rebuild the labels box.
         for w in self.catbox.get_children():
@@ -89,7 +91,7 @@ class ChartScreen(Gtk.HBox):
 
         catgroup = Gtk.SizeGroup(Gtk.SizeGroupMode.HORIZONTAL)
         amountgroup = Gtk.SizeGroup(Gtk.SizeGroupMode.HORIZONTAL)
-        
+
         for c in self.sorted_categories:
             hbox = Gtk.HBox()
 
@@ -126,9 +128,9 @@ class ChartScreen(Gtk.HBox):
         # Draw pie chart.
         bounds = widget.get_allocation()
 
-        x = bounds.width/2
-        y = bounds.height/2
-        r = min(bounds.width, bounds.height)/2 - 10 
+        x = bounds.width / 2
+        y = bounds.height / 2
+        r = min(bounds.width, bounds.height) / 2 - 10
 
         total = 0
         for c in self.sorted_categories:
@@ -138,9 +140,9 @@ class ChartScreen(Gtk.HBox):
             angle = 0.0
 
             for c in self.sorted_categories:
-                slice = 2*math.pi * self.category_total[c] / total
+                slice = 2 * math.pi * self.category_total[c] / total
                 color = finance.get_category_color(c)
- 
+
                 context.move_to(x, y)
                 context.arc(x, y, r, angle, angle + slice)
                 context.close_path()

budgetscreen.py
@@ -1,38 +1,40 @@
-# Copyright 2008 by Wade Brainerd.  
+# Copyright 2008 by Wade Brainerd.
 # This file is part of Finance.
 #
 # Finance is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Finance is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Finance.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import standard Python modules.
-import logging, os, math, time, copy, json, time, datetime, locale
-from gettext import gettext as _
+import datetime
+import locale
 
-# Set up localization.
-locale.setlocale(locale.LC_ALL, '')
+from gettext import gettext as _
 
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
 
-# Import Sugar UI modules.
-import sugar3.activity.activity
-from sugar3.graphics import *
+# Set up localization.
+locale.setlocale(locale.LC_ALL, '')
 
 # Import activity module
 import finance
 
-BUDGET_HELP = _('The Budget view allows you to set a monthly budget for each expense category, and to keep track of your\nbudgets.  To set a budget, type the amount in the box to the right of the category.')
+BUDGET_HELP = _(
+    'The Budget view allows you to set a monthly budget for each expense '
+    'category, and to keep track of your\nbudgets. To set a budget, type '
+    'the amount in the box to the right of the category.')
+
 
 class BudgetScreen(Gtk.VBox):
     def __init__(self, activity):
@@ -42,7 +44,7 @@ class BudgetScreen(Gtk.VBox):
 
         self.category_total = {}
         self.sorted_categories = []
- 
+
         self.budgetbox = Gtk.VBox()
 
         scroll = Gtk.ScrolledWindow()
@@ -57,12 +59,12 @@ class BudgetScreen(Gtk.VBox):
         for t in self.activity.visible_transactions:
             cat = t['category']
             amount = t['amount']
-            
+
             if t['type'] == 'debit':
-                if not self.category_total.has_key(cat):
+                if not cat in self.category_total:
                     self.category_total[cat] = amount
-                else: 
-                    self.category_total[cat] += amount 
+                else:
+                    self.category_total[cat] += amount
 
         # Generate a list of names sorted by total.
         self.sorted_categories = self.category_total.keys()
@@ -74,12 +76,12 @@ class BudgetScreen(Gtk.VBox):
 
         # Build header.
         catlabel = Gtk.Label()
-        catlabel.set_markup('<b><big>'+_('Category')+'</big></b>')
+        catlabel.set_markup('<b><big>' + _('Category') + '</big></b>')
         spentlabel = Gtk.Label()
-        spentlabel.set_markup('<b><big>'+_('Spent')+'</big></b>')
+        spentlabel.set_markup('<b><big>' + _('Spent') + '</big></b>')
         budgetlabel = Gtk.Label()
-        budgetlabel.set_markup('<b><big>'+_('Budget')+'</big></b>')
-        
+        budgetlabel.set_markup('<b><big>' + _('Budget') + '</big></b>')
+
         headerbox = Gtk.HBox()
         headerbox.pack_start(catlabel, False, True, 20)
         headerbox.pack_start(spentlabel, True, True, 10)
@@ -94,7 +96,7 @@ class BudgetScreen(Gtk.VBox):
 
         budgetgroup = Gtk.SizeGroup(Gtk.SizeGroupMode.HORIZONTAL)
         budgetgroup.add_widget(budgetlabel)
-        
+
         # Build categories.
         for c in self.sorted_categories:
             description = c
@@ -120,7 +122,7 @@ class BudgetScreen(Gtk.VBox):
             budgetentry = Gtk.Entry()
             budgetentry.connect('changed', self.budget_changed_cb, c)
             budgetentry.set_width_chars(10)
-            if self.activity.data['budgets'].has_key(c):
+            if c in self.activity.data['budgets']:
                 b = self.activity.data['budgets'][c]
                 budgetentry.set_text(locale.currency(b['amount'], False))
             budgetgroup.add_widget(budgetentry)
@@ -141,7 +143,7 @@ class BudgetScreen(Gtk.VBox):
             self.budgetbox.pack_start(hbox, False, False, 5)
 
         self.show_all()
-            
+
         self.activity.set_help(BUDGET_HELP)
 
     def bar_draw_cb(self, widget, cr, category):
@@ -149,13 +151,18 @@ class BudgetScreen(Gtk.VBox):
 
         # Draw amount of time spent in period if sensible.
         period_ratio = None
-        if self.activity.period != _('Day') and self.activity.period != _('Forever'):
-            period_length = (self.activity.get_next_period(self.activity.period_start) - self.activity.period_start).days
-            period_ratio = float((datetime.date.today() - self.activity.period_start).days) / period_length
+        if self.activity.period != _('Day') and \
+                self.activity.period != _('Forever'):
+            period_length = (
+                self.activity.get_next_period(self.activity.period_start) - \
+                    self.activity.period_start).days
+            period_ratio = float(
+                (datetime.date.today() - self.activity.period_start).days) / \
+                period_length
 
             if period_ratio > 0:
                 cr.set_source_rgb(0.9, 0.9, 0.9)
-                cr.rectangle(0, 0, bounds.width*period_ratio, bounds.height)
+                cr.rectangle(0, 0, bounds.width * period_ratio, bounds.height)
                 cr.fill()
 
         # Draw outline.
@@ -166,7 +173,7 @@ class BudgetScreen(Gtk.VBox):
         # Draw arrow and cost.
         total = self.category_total[category]
 
-        if self.activity.data['budgets'].has_key(category):
+        if category in self.activity.data['budgets']:
             budget = self.activity.data['budgets'][category]['amount']
 
             # Convert from monthly budget.
@@ -182,10 +189,10 @@ class BudgetScreen(Gtk.VBox):
             ratio = total / budget
 
             cr.move_to(5, 5)
-            cr.line_to(ratio * (bounds.width-30), 5)
-            cr.line_to(ratio * (bounds.width-5), bounds.height/2)
-            cr.line_to(ratio * (bounds.width-30), bounds.height-5)
-            cr.line_to(5, bounds.height-5)
+            cr.line_to(ratio * (bounds.width - 30), 5)
+            cr.line_to(ratio * (bounds.width - 5), bounds.height / 2)
+            cr.line_to(ratio * (bounds.width - 30), bounds.height - 5)
+            cr.line_to(5, bounds.height - 5)
             cr.close_path()
 
             if ratio > 1.0:
@@ -195,7 +202,7 @@ class BudgetScreen(Gtk.VBox):
             else:
                 cr.set_source_rgb(0.6, 1.0, 0.6)
             cr.fill_preserve()
-            
+
             cr.set_source_rgb(0.5, 0.5, 0.5)
             cr.stroke()
 
@@ -205,14 +212,14 @@ class BudgetScreen(Gtk.VBox):
 
         cr.set_font_size(20)
         x_bearing, y_bearing, width, height = cr.text_extents(text)[:4]
-    
-        cr.move_to(20, (bounds.height-height)/2 - y_bearing)
+
+        cr.move_to(20, (bounds.height - height) / 2 - y_bearing)
         cr.show_text(text)
 
     def budget_changed_cb(self, widget, category):
         text = widget.get_text()
         if text != '':
-            self.activity.data['budgets'][category] = { 'amount': float(text) }
+            self.activity.data['budgets'][category] = {'amount': float(text)}
         else:
-            del self.activity.data['budgets'][category] 
+            del self.activity.data['budgets'][category]
         self.queue_draw()

registerscreen.py
@@ -1,37 +1,41 @@
-# Copyright 2008 by Wade Brainerd.  
+# Copyright 2008 by Wade Brainerd.
 # This file is part of Finance.
 #
 # Finance is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Finance is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Finance.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import standard Python modules.
-import logging, os, math, time, copy, time, datetime, locale
+import time
+import datetime
+import locale
 from gettext import gettext as _
-
-# Set up localization.
-locale.setlocale(locale.LC_ALL, '')
 
 from gi.repository import Gtk
 from gi.repository import GObject
 
-# Import Sugar UI modules.
-import sugar3.activity.activity
-from sugar3.graphics import *
+# Set up localization.
+locale.setlocale(locale.LC_ALL, '')
 
 # Import activity module
 import finance
 
-REGISTER_HELP = _('<b>Welcome to Finance!</b>   This activity keeps track of income and expenses for anything that earns\nor spends money, like a school club.  To get started, use the Transaction box to add credits and debits.\nOnce you have entered some transactions, visit the Chart and Budget views to see more.')
+REGISTER_HELP = _(
+    '<b>Welcome to Finance!</b>   This activity keeps track of income '
+    'and expenses for anything that earns\nor spends money, like a school '
+    'club.  To get started, use the Transaction box to add credits and '
+    'debits.\nOnce you have entered some transactions, visit the Chart '
+    'and Budget views to see more.')
+
 
 class RegisterScreen(Gtk.VBox):
     def __init__(self, activity):
@@ -44,18 +48,20 @@ class RegisterScreen(Gtk.VBox):
         self.treeview.set_rules_hint(True)
         self.treeview.set_enable_search(False)
 
-        # Note that the only thing we store in our liststore is the transaction id.
-        # All the actual data is in the activity database.
+        # Note that the only thing we store in our liststore is the
+        # transaction id. All the actual data is in the activity
+        # database.
         self.liststore = Gtk.ListStore(GObject.TYPE_INT)
         self.treeview.set_model(self.liststore)
 
         # Construct the columns.
         renderer = Gtk.CellRendererText()
         renderer.props.editable = True
-        renderer.connect('editing-started', self.description_editing_started_cb)
+        renderer.connect('editing-started',
+                         self.description_editing_started_cb)
         renderer.connect('edited', self.description_edit_cb)
         col = Gtk.TreeViewColumn(_('Description'), renderer)
-        col.set_cell_data_func(renderer, self.description_render_cb) 
+        col.set_cell_data_func(renderer, self.description_render_cb)
         col.set_expand(True)
         self.treeview.append_column(col)
 
@@ -63,7 +69,7 @@ class RegisterScreen(Gtk.VBox):
         renderer.props.editable = True
         renderer.connect('edited', self.amount_edit_cb)
         col = Gtk.TreeViewColumn(_('Amount'), renderer)
-        col.set_cell_data_func(renderer, self.amount_render_cb) 
+        col.set_cell_data_func(renderer, self.amount_render_cb)
         col.set_alignment(0.5)
         col.set_min_width(120)
         self.treeview.append_column(col)
@@ -73,7 +79,7 @@ class RegisterScreen(Gtk.VBox):
         renderer.connect('edited', self.date_edit_cb)
         col = Gtk.TreeViewColumn(_('Date'), renderer)
         col.set_alignment(0.5)
-        col.set_cell_data_func(renderer, self.date_render_cb) 
+        col.set_cell_data_func(renderer, self.date_render_cb)
         col.set_min_width(150)
         self.treeview.append_column(col)
 
@@ -82,7 +88,7 @@ class RegisterScreen(Gtk.VBox):
         renderer.connect('editing-started', self.category_editing_started_cb)
         renderer.connect('edited', self.category_edit_cb)
         col = Gtk.TreeViewColumn(_('Category'), renderer)
-        col.set_cell_data_func(renderer, self.category_render_cb) 
+        col.set_cell_data_func(renderer, self.category_render_cb)
         col.set_alignment(0.5)
         col.set_min_width(300)
         self.treeview.append_column(col)
@@ -124,8 +130,9 @@ class RegisterScreen(Gtk.VBox):
         t = self.activity.transaction_map[id]
         t['name'] = new_text
 
-        # Automatically fill in category if empty, and if transaction name is known.
-        if t['category'] == '' and self.activity.transaction_names.has_key(new_text):
+        # Automatically fill in category if empty, and if transaction
+        # name is known.
+        if t['category'] == '' and new_text in self.activity.transaction_names:
             for ct in self.activity.data['transactions']:
                 if ct['name'] == new_text and ct['category'] != '':
                     t['category'] = ct['category']
@@ -136,10 +143,12 @@ class RegisterScreen(Gtk.VBox):
         cell_renderer.set_property('xalign', 1.0)
         if t['type'] == 'credit':
             cell_renderer.set_property('foreground', '#4040ff')
-            cell_renderer.set_property('text', locale.currency(t['amount'], False))
+            cell_renderer.set_property('text',
+                                       locale.currency(t['amount'], False))
         else:
             cell_renderer.set_property('foreground', '#ff4040')
-            cell_renderer.set_property('text', locale.currency(-t['amount'], False))
+            cell_renderer.set_property('text',
+                                       locale.currency(-t['amount'], False))
 
     def amount_edit_cb(self, cell_renderer, path, new_text):
         id = self.liststore[path][0]
@@ -166,7 +175,8 @@ class RegisterScreen(Gtk.VBox):
         id = model.get_value(iter, 0)
         t = self.activity.transaction_map[id]
         cell_renderer.set_property('text', t['category'])
-        cell_renderer.set_property('background', finance.get_category_color_str(t['category']))
+        cell_renderer.set_property(
+            'background', finance.get_category_color_str(t['category']))
 
     def category_editing_started_cb(self, cell_renderer, editable, path):
         completion = Gtk.EntryCompletion()
@@ -185,7 +195,7 @@ class RegisterScreen(Gtk.VBox):
         t = self.activity.transaction_map[id]
         t['category'] = new_text
         if new_text != '':
-            self.activity.category_names[new_text] = 1 
+            self.activity.category_names[new_text] = 1
 
     def newcredit_cb(self, widget):
         # Automatically display the register screen.
@@ -196,8 +206,9 @@ class RegisterScreen(Gtk.VBox):
         id = self.activity.create_transaction(_('New Credit'), 'credit', 0)
         iter = self.liststore.append((id,))
         # Set cursor and begin editing the description.
-        self.treeview.set_cursor(self.liststore.get_path(iter), self.treeview.get_column(0), True)
-        
+        self.treeview.set_cursor(self.liststore.get_path(iter),
+                                 self.treeview.get_column(0), True)
+
     def newdebit_cb(self, widget):
         # Automatically display the register screen.
         if self.activity.screens[-1] != self.activity.register:
@@ -207,10 +218,11 @@ class RegisterScreen(Gtk.VBox):
         id = self.activity.create_transaction(_('New Debit'), 'debit', 0)
         iter = self.liststore.append((id,))
         # Set cursor and begin editing the description.
-        self.treeview.set_cursor(self.liststore.get_path(iter), self.treeview.get_column(0), True)
-        
+        self.treeview.set_cursor(self.liststore.get_path(iter),
+                                 self.treeview.get_column(0), True)
+
     def eraseitem_cb(self, widget):
-        # Ignore unless on the register screen. 
+        # Ignore unless on the register screen.
         if self.activity.screens[-1] != self.activity.register:
             return
 
@@ -227,6 +239,6 @@ class RegisterScreen(Gtk.VBox):
             # Select the next item, or else the last item.
             sel.select_path(path)
             if not sel.path_is_selected(path):
-               row = path[0]-1
-               if row >= 0: 
-                   sel.select_path((row,))
+                row = path[0] - 1
+                if row >= 0:
+                    sel.select_path((row,))

setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 from sugar3.activity import bundlebuilder
+
+
 if __name__ == "__main__":
     bundlebuilder.start()

commit 3e89f0c25ec8bb2ebaa76f66155ef8b283efca36

Bug fix
These two bugs were introduced in 1c21ce44b81831132aaa520fdde4e8eccce1b705

Signed-off-by: Manuel Kaufmann <humitos@gmail.com>

chartscreen.py
@@ -75,7 +75,7 @@ class ChartScreen(Gtk.HBox):
             amount = t['amount']
 
             if t['type'] == 'debit':
-                if not cat self.category_total:
+                if cat not in self.category_total:
                     self.category_total[cat] = amount
                 else:
                     self.category_total[cat] += amount

finance.py
@@ -43,7 +43,7 @@ from sugar3.graphics.toolbarbox import ToolbarButton
 from sugar3.graphics.radiotoolbutton import RadioToolButton
 from sugar3.activity.widgets import StopButton
 from sugar3.activity.widgets import ActivityToolbarButton
-from sugar3.activity import Activity
+from sugar3.activity.activity import Activity
 
 # Set up localization.
 locale.setlocale(locale.LC_ALL, '')