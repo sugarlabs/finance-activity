c5720c35d468695274135009134689b26eb8c568
James Cameron <quozl@laptop.org>
2020-02-22 09:01:26 +1100

    Review - consistent indentation style

--- extractinput.py
+++ extractinput.py
@@ -11,41 +11,41 @@ from sugar3.graphics.icon import Icon
 
 
 def evaluate(value):
-        if isinstance(value, str):
+    if isinstance(value, str):
 
-            binOps = {
-                ast.Add: operator.add,
-                ast.Sub: operator.sub,
-                ast.Mult: operator.mul,
-                ast.Div: operator.truediv,
-            }
+        binOps = {
+            ast.Add: operator.add,
+            ast.Sub: operator.sub,
+            ast.Mult: operator.mul,
+            ast.Div: operator.truediv,
+        }
 
-            try:
-                node = ast.parse(value, mode='eval')
-            except:
-                return None
+        try:
+            node = ast.parse(value, mode='eval')
+        except:
+            return None
 
-            def _eval(node):
-                if isinstance(node, ast.Expression):
-                    return _eval(node.body)
-                elif isinstance(node, ast.BinOp):
-                    return binOps[type(node.op)](_eval(node.left), _eval(node.right))
-                elif isinstance(node, ast.Num):
-                    return node.n
-                else:
-                    return None
+        def _eval(node):
+            if isinstance(node, ast.Expression):
                 return _eval(node.body)
+            elif isinstance(node, ast.BinOp):
+                return binOps[type(node.op)](_eval(node.left), _eval(node.right))
+            elif isinstance(node, ast.Num):
+                return node.n
+            else:
+                return None
+            return _eval(node.body)
 
-            value = _eval(node)
+        value = _eval(node)
 
-        decimals_found = re.findall("\d+\.\d+", str(value))
-        integers_found = re.findall("\d+", str(value))
+    decimals_found = re.findall("\d+\.\d+", str(value))
+    integers_found = re.findall("\d+", str(value))
 
-        if decimals_found != []:
-            return decimals_found[0]
-        elif integers_found != []:
-            return integers_found[0]
-        return None
+    if decimals_found != []:
+        return decimals_found[0]
+    elif integers_found != []:
+        return integers_found[0]
+    return None
 
 
 def _invalid_number_alert(activity):
