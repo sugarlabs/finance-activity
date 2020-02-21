4dca761340466023a1537d9742b0b43be697b1e8
James Cameron <quozl@laptop.org>
2020-02-22 10:13:24 +1100

    Review - add parser test

--- parse.py
+++ parse.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from gi.repository import Gtk
 import locale
 import ast
@@ -67,3 +68,28 @@ def invalid_value_alert(activity):
     alert.connect('response', lambda a, r: activity.remove_alert(a))
     activity.add_alert(alert)
     alert.show()
+
+
+if __name__ == "__main__":
+    tests = [
+        ['0', 0.0],
+        ['0.55', 0.55],
+        ['1', 1.0],
+        ['2', 2.0],
+        ['-2', -2.0],
+        ['2+2', 4.0],
+        ['4-2', 2.0],
+        ['4*2', 8.0],
+        ['4/2', 2.0],
+        #  ['4*-4', -4.0],  # TypeError: 
+        ['4/2-1', 1.0],
+        ['1.5', 1.5],
+        ['-1.7', -1.7],
+    ]
+    for test in tests:
+        text, expected = test
+        observed = evaluate(text)
+        if observed != expected:
+            print('fail; %r -> %r instead of %r' % (text, observed, expected))
+        else:
+            print('pass; %r -> %r' % (text, observed))
